0b5f9fb3af3cd226ba60d134db0e96d7546cbae7
prubio321 <p.rubioh321@gmail.com>
2022-06-01 20:33:09 -0400

    ObjetoAlumno

--- src/com/generation/f20220530/Alumno.java
+++ src/com/generation/f20220530/Alumno.java
@@ -6,7 +6,7 @@ public class Alumno {//Cuando es una coleccion ahi se recomienda la clase en plu
     private String apellido;
     private int edad;
     private String curso;
-    //protected podemos acceder entre las clases del package
+    //protected podemos acceder entre las clases del package.
     //private solo accedemos dentro de la misma clase
     //encapsulamiento podemos proteger la informacion, no cualquiera va a tener acceso a los atributos del alumno
     //public todos pueden acceder
